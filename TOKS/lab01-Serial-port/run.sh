12d32201d5c91c634f93f005e42c0cb62827c0cf
Andrey Sikorin <amaomi.prog@gmail.com>
2022-09-29 22:17:13 +0300

    Move `sudo` from tmux to commands

--- TOKS/lab01-Serial-port/run.sh
+++ TOKS/lab01-Serial-port/run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-sudo tmux \
-  new-session  "python setup.py /dev/tnt0 write ; read" \; \
-  split-window "python setup.py /dev/tnt1 read ; read" \; \
-  split-window "python setup.py /dev/tnt1 write ; read" \; \
-  split-window "python setup.py /dev/tnt0 read ; read" \; \
+tmux \
+  new-session  "sudo python setup.py /dev/tnt0 write ; read" \; \
+  split-window "sudo python setup.py /dev/tnt1 read ; read" \; \
+  split-window "sudo python setup.py /dev/tnt1 write ; read" \; \
+  split-window "sudo python setup.py /dev/tnt0 read ; read" \; \
   select-layout even-horizontal
