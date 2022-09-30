2e7659491d64a5175b16c5027773a9f389de9622
Andrey Sikorin <amaomi.prog@gmail.com>
2022-09-30 11:09:21 +0300

    Fix issue with `>\n` message

--- TOKS/lab01-Serial-port/stuffing.py
+++ TOKS/lab01-Serial-port/stuffing.py
@@ -11,4 +11,4 @@ def encode_message(message: str):
 def decode_message(message: str):
     message = message[1:]
     parts = message.split(FLAG_BYTE)
-    return "".join([parts[0]] + [FLAG_BYTE + x[1:] for x in parts if len(x) > 0 and x[0] == ESC_CHAR])
+    return "".join([parts[0]] + [FLAG_BYTE + x[1:] for x in parts[1:] if len(x) > 0 and x[0] == ESC_CHAR])
