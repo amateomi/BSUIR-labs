ddb7720cd0d784c607c2ce530f0deb9a68e11b6d
Andrey Sikorin <amaomi.prog@gmail.com>
2022-10-10 14:43:23 +0300

    Swap `setup.py` to `portio.py`

--- TOKS/lab01-Serial-port/run.sh
+++ TOKS/lab01-Serial-port/run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 tmux \
-  new-session  "sudo python setup.py /dev/tnt0 write ; read" \; \
-  split-window "sudo python setup.py /dev/tnt1 read ; read" \; \
-  split-window "sudo python setup.py /dev/tnt1 write ; read" \; \
-  split-window "sudo python setup.py /dev/tnt0 read ; read" \; \
+  new-session  "sudo python portio.py /dev/tnt0 write ; read" \; \
+  split-window "sudo python portio.py /dev/tnt1 read ; read" \; \
+  split-window "sudo python portio.py /dev/tnt1 write ; read" \; \
+  split-window "sudo python portio.py /dev/tnt0 read ; read" \; \
   select-layout even-horizontal
