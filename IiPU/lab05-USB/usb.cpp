764003bebf247da32162def3e7532f9434e7b48e
Andrey Sikorin <amaomi.prog@gmail.com>
2022-11-27 19:44:51 +0300

    Simplify lab05

--- IiPU/lab05-USB/usb.cpp
+++ IiPU/lab05-USB/usb.cpp
@@ -1,78 +1,31 @@
-#include <stdio.h>
-#include <unistd.h>
 #include <libudev.h>
 #include <sys/mount.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <sys/inotify.h>
-#include <stdlib.h>
-#include <cstring>
+#include <unistd.h>
 
-#include <algorithm>
-#include <list>
-#include <string>
 #include <filesystem>
-#include <iostream>
-#include <future>
-#include <map>
 #include <fstream>
+#include <iostream>
+#include <list>
+#include <map>
 #include <thread>
-#include <memory>
-#include <future>
 
 using namespace std;
 using namespace filesystem;
 
-constexpr auto MAX_EVENTS = 64;
-constexpr auto DRIVE = 1;
-constexpr auto MOUSE = 2;
+enum class DeviceType {
+    Drive = 1,
+    Mouse = 2,
+};
 
 const auto MOUNT_LIST = "/proc/mounts";
 const auto INTERFACE_TYPE = "bNumInterfaces";
-const auto MOUNT_DIR = "/run/media/" + string{getlogin()};
-
-class SmartWatcher {
-public:
-    SmartWatcher(int inotifyFd, const path& path)
-            : m_inotifyFd{inotifyFd},
-              m_watcherFd{inotify_add_watch(inotifyFd, path.c_str(), IN_OPEN | IN_CLOSE)} {
-        if (m_watcherFd < 0) {
-            perror("add_watch");
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    ~SmartWatcher() {
-        inotify_rm_watch(m_inotifyFd, m_watcherFd);
-    }
-
-    void onEvent(const inotify_event& event) {
-        if (event.wd == m_watcherFd) {
-            if (event.mask & IN_OPEN) {
-                ++m_openCloseCounter;
-                cout << "Open event: " << m_watcherFd << ", counter: " << m_openCloseCounter << endl;
-            }
-            if (event.mask & IN_CLOSE) {
-                --m_openCloseCounter;
-                cout << "Close event: " << m_watcherFd << ", counter: " << m_openCloseCounter << endl;
-            }
-        }
-    }
-
-    [[nodiscard]] bool isSafeToUnmount() const { return m_openCloseCounter == 0; }
-
-private:
-    int m_inotifyFd{};
-    int m_watcherFd{};
-    int m_openCloseCounter{};
-};
 
 class UsbDeviceManager {
 public:
     UsbDeviceManager()
-            : m_udev{udev_new()},
-              m_monitor{udev_monitor_new_from_netlink(m_udev, "udev")},
-              m_inotifyFd{inotify_init()} {
+        : m_udev { udev_new() }
+        , m_monitor { udev_monitor_new_from_netlink(m_udev, "udev") }
+    {
         if (!m_udev) {
             cerr << "udev_new: failed" << endl;
             exit(EXIT_FAILURE);
@@ -81,34 +34,53 @@ public:
         udev_monitor_enable_receiving(m_monitor);
     }
 
-    ~UsbDeviceManager() {
-        udev_unref(m_udev);
-    }
+    ~UsbDeviceManager() { udev_unref(m_udev); }
 
-    [[noreturn]] void observe() {
+    [[noreturn]] void observe()
+    {
         while (true) {
-            if (auto dev = udev_monitor_receive_device(m_monitor);
-                    dev) {
-                if (string action{udev_device_get_action(dev)};
-                        action == "add" || action == "remove") {
-                    string port{udev_device_get_sysname(dev)};
-                    string devicePath{udev_device_get_syspath(dev)};
+            if (auto dev = udev_monitor_receive_device(m_monitor); dev) {
 
+                string action { udev_device_get_action(dev) };
+                string port { udev_device_get_sysname(dev) };
+                string devicePath { udev_device_get_syspath(dev) };
+                DeviceType deviceType {};
+                path driveMountPath;
+
+                if (action == "add") {
+                    int type;
+                    ifstream { devicePath + "/" + INTERFACE_TYPE } >> type;
+                    deviceType = static_cast<DeviceType>(type);
+
+                    if (deviceType == DeviceType::Drive) {
+                        // Wait to sync with kernel
+                        sleep(3);
+                        ifstream mountList { MOUNT_LIST };
+
+                        string lastLine, temp;
+                        do {
+                            lastLine = temp;
+                            getline(mountList, temp);
+                        } while (mountList);
+
+                        istringstream iss { lastLine };
+                        // Skip first word
+                        iss >> m_drivePortToMount[port] >> m_drivePortToMount[port];
+                        driveMountPath = m_drivePortToMount[port];
+                    }
+                } else if (action == "remove") {
+                    deviceType = m_drivePortToMount.count(port) == 1 ? DeviceType::Drive : DeviceType::Mouse;
+                    driveMountPath = m_drivePortToMount[port];
+                    m_drivePortToMount.erase(port);
+                }
+
+                if (action == "add" || action == "remove") {
                     cout << endl
                          << "Action: " << action << endl
                          << "Port name: " << port << endl
                          << "Device path: " << devicePath << endl;
-
-                    int deviceType{};
-                    if (action == "add")
-                        ifstream{devicePath + "/" + INTERFACE_TYPE} >> deviceType;
-                    else if (action == "remove")
-                        deviceType = m_drives.count(port) == 1 ? DRIVE : MOUSE;
-
-                    if (deviceType == DRIVE) {
-                        handleDriveAction(port, action);
-                    }
-
+                    if (deviceType == DeviceType::Drive)
+                        cout << "Drive mount path: " << driveMountPath << endl;
                     cout << endl;
                 }
                 udev_device_unref(dev);
@@ -116,97 +88,27 @@ public:
         }
     }
 
-    bool isSafeToUnmount(const string& port) {
-        return all_of(m_drives[port].watchers.begin(),
-                      m_drives[port].watchers.end(),
-                      [](const auto& watcher) {
-                          return watcher.isSafeToUnmount();
-                      });
-    }
-
-    void unmount(const string& port) {
-        auto res = umount(m_drives[port].mountPath.c_str());
-        if (res < 0) {
-            perror("umount");
-            exit(EXIT_FAILURE);
+    void unmount(const string& port, int flag)
+    {
+        auto res = umount2(m_drivePortToMount[port].c_str(), flag);
+        if (res < 0 && errno == EBUSY) {
+            cout << "USB drive is busy right now" << endl;
+        } else if (res < 0) {
+            perror("umount2");
+            exit(errno);
         }
     }
 
 private:
-    /// Return mount path if USB drive
-    void handleDriveAction(const string& port, const string& action) {
-        if (action == "add") {
-            // Wait to sync with kernel
-            sleep(3);
-            ifstream mountList{MOUNT_LIST};
-
-            string lastLine, temp;
-            do {
-                lastLine = temp;
-                getline(mountList, temp);
-            } while (mountList);
-
-            istringstream iss{lastLine};
-            path mountPath;
-            // Skip first word
-            iss >> mountPath >> mountPath;
-            m_drives[port].mountPath = mountPath;
-
-            // Add notifiers
-            for (const auto& file: directory_iterator{m_drives[port].mountPath})
-                m_drives[port].watchers.emplace_back(m_inotifyFd, file.path());
-
-            m_drives[port].eventHandler = new thread{&UsbDeviceManager::OnEvent, this, port};
-
-            cout << "USB drive mount path: " << m_drives[port].mountPath << endl;
-
-        } else if (action == "remove") {
-            cout << "USB drive mount path: " << m_drives[port].mountPath << endl;
-            m_drives[port].stopHandler = true;
-            m_drives[port].eventHandler->join();
-            delete m_drives[port].eventHandler;
-            m_drives.erase(port);
-        }
-    }
-
-    void OnEvent(const string& port) {
-        inotify_event events[MAX_EVENTS];
-        while (!m_drives[port].stopHandler) {
-            memset(events, 0, sizeof(events));
-            auto numEvents = read(m_inotifyFd, events, sizeof(events));
-            if (numEvents < 0) {
-                perror("read");
-                exit(EXIT_FAILURE);
-            }
-            for (auto i = 0; i < numEvents; ++i) {
-                auto event = events[i];
-                for (auto& watcher: m_drives[port].watchers) {
-                    watcher.onEvent(event);
-                }
-            }
-        }
-    }
-
-    udev* m_udev{};
-    udev_monitor* m_monitor{};
-    int m_inotifyFd{};
-
-    struct DriveUtility {
-        path mountPath;
-        list<SmartWatcher> watchers;
-        thread* eventHandler{};
-        bool stopHandler{};
-    };
-    map<string, DriveUtility> m_drives;
+    udev* m_udev {};
+    udev_monitor* m_monitor {};
+    map<string, path> m_drivePortToMount;
 };
 
-int main() {
-    cin.tie(nullptr);
-    ios::sync_with_stdio(false);
-
+int main()
+{
     UsbDeviceManager manager;
-//    async(&UsbDeviceManager::observe, &manager);
-    thread observer{&UsbDeviceManager::observe, &manager};
+    thread observer { &UsbDeviceManager::observe, &manager };
     while (true) {
         cout << "Enter option: " << endl
              << "1) - Safe unmount" << endl
@@ -219,12 +121,9 @@ int main() {
             string port;
             getline(cin, port);
             if (option == "1") {
-                if (manager.isSafeToUnmount(port))
-                    manager.unmount(port);
-                else
-                    cout << "Unsafe to unmount" << endl;
+                manager.unmount(port, 0);
             } else {
-                manager.unmount(port);
+                manager.unmount(port, MNT_DETACH);
             }
         } else if (option == "q") {
             exit(EXIT_SUCCESS);
