ed5891e5632e8c583ded35875b0f105192e7838f
Andrey Sikorin <amaomi.prog@gmail.com>
2023-01-29 18:35:37 +0300

    Finish basic lab

--- AVP/lab01-Vectorization/src/main.rs
+++ AVP/lab01-Vectorization/src/main.rs
@@ -1,10 +1,10 @@
 #![allow(non_snake_case)]
 
 use std::arch::x86_64::*;
-use std::time::Instant;
+use std::time::{Instant};
 
-const LITTLE_SIZE: usize = 32;
-const SIZE: usize = 2;
+const LITTLE_SIZE: usize = 16;
+const SIZE: usize = 32;
 
 type Matrix = [[f32; LITTLE_SIZE]; LITTLE_SIZE];
 type MegaMatrix = [[Matrix; SIZE]; SIZE];
@@ -26,7 +26,21 @@ fn new_mega_matrix() -> Box<MegaMatrix> {
     result
 }
 
-fn multiply_matrices(m1: &Matrix, m2: &Matrix) -> Matrix {
+fn compare_mega_matrices(m1: &MegaMatrix, m2: &MegaMatrix) -> bool {
+    for i in 0..SIZE {
+        for j in 0..SIZE {
+            for k in 0..LITTLE_SIZE {
+                for l in 0..LITTLE_SIZE {
+                    if f32::abs(m1[i][j][k][l] - m2[i][j][k][l]) > 0.001 { return false; }
+                }
+            }
+        }
+    }
+
+    true
+}
+
+fn mul_no_simd(m1: &Matrix, m2: &Matrix) -> Matrix {
     let mut result: Matrix = [[0f32; LITTLE_SIZE]; LITTLE_SIZE];
     for i in 0..m1.len() {
         for j in 0..m1.len() {
@@ -38,30 +52,7 @@ fn multiply_matrices(m1: &Matrix, m2: &Matrix) -> Matrix {
     result
 }
 
-fn multiply_matrices_avx(m1: &Matrix, m2: &Matrix) -> Matrix {
-    let mut result: Matrix = [[0f32; LITTLE_SIZE]; LITTLE_SIZE];
-    let mut buffer = [0f32; 8];
-    for i in 0..m1.len() {
-        for j in 0..m1.len() {
-            unsafe {
-                let mut accumulator = _mm256_set1_ps(0f32);
-                for k in (0..m1.len()).step_by(8) {
-                    let ymm0 = _mm256_loadu_ps(&m1[i][k]);
-                    let ymm1 = _mm256_set_ps(m2[k + 7][j], m2[k + 6][j],
-                                             m2[k + 5][j], m2[k + 4][j],
-                                             m2[k + 3][j], m2[k + 2][j],
-                                             m2[k + 1][j], m2[k + 0][j]);
-                    accumulator = _mm256_fmadd_ps(ymm0, ymm1, accumulator);
-                }
-                _mm256_storeu_ps(buffer.as_mut_ptr(), accumulator);
-            }
-            result[i][j] = buffer.iter().sum();
-        }
-    }
-    result
-}
-
-fn sum_matrices(result: &mut Matrix, m: &Matrix) {
+fn sum_no_simd(result: &mut Matrix, m: Matrix) {
     for i in 0..m.len() {
         for j in 0..m.len() {
             result[i][j] += m[i][j];
@@ -69,57 +60,145 @@ fn sum_matrices(result: &mut Matrix, m: &Matrix) {
     }
 }
 
-fn compute(result: &mut MegaMatrix, m1: &MegaMatrix, m2: &MegaMatrix) {
-    for i in 0..m1.len() {
-        for j in 0..m1.len() {
-            for k in 0..m1.len() {
-                sum_matrices(&mut result[i][j], &multiply_matrices(&m1[i][k], &m2[k][j]));
+fn compute_no_simd(result: &mut MegaMatrix, m1: &MegaMatrix, m2: &MegaMatrix) {
+    for i in 0..result.len() {
+        for j in 0..result.len() {
+            for k in 0..result.len() {
+                sum_no_simd(&mut result[i][j], mul_no_simd(&m1[i][k], &m2[k][j]));
             }
         }
     }
 }
 
-fn compute_avx(result: &mut MegaMatrix, m1: &MegaMatrix, m2: &MegaMatrix) {
+#[target_feature(enable = "sse,avx")]
+unsafe fn mul_auto_simd(m1: &Matrix, m2: &Matrix) -> Matrix {
+    let mut result: Matrix = [[0f32; LITTLE_SIZE]; LITTLE_SIZE];
     for i in 0..m1.len() {
         for j in 0..m1.len() {
             for k in 0..m1.len() {
-                sum_matrices(&mut result[i][j], &multiply_matrices(&m1[i][k], &m2[k][j]));
+                result[i][j] += m1[i][k] * m2[k][j];
+            }
+        }
+    }
+    result
+}
+
+#[target_feature(enable = "sse,avx")]
+unsafe fn sum_auto_simd(result: &mut Matrix, m: Matrix) {
+    for i in 0..m.len() {
+        for j in 0..m.len() {
+            result[i][j] += m[i][j];
+        }
+    }
+}
+
+fn compute_auto_simd(result: &mut MegaMatrix, m1: &MegaMatrix, m2: &MegaMatrix) {
+    for i in 0..result.len() {
+        for j in 0..result.len() {
+            for k in 0..result.len() {
+                unsafe {
+                    sum_auto_simd(&mut result[i][j], mul_auto_simd(&m1[i][k], &m2[k][j]));
+                }
             }
         }
     }
 }
 
-fn compare_matrices(m1: &Matrix, m2: &Matrix) -> bool {
-    for i in 0..LITTLE_SIZE {
-        for j in 0..LITTLE_SIZE {
-            if f32::abs(m1[i][j] - m2[i][j]) > 0.0001 { return false; }
+#[target_feature(enable = "sse,avx")]
+unsafe fn mul_my_simd(m1: &Matrix, m2: &Matrix) -> Matrix {
+    let mut result: Matrix = [[0f32; LITTLE_SIZE]; LITTLE_SIZE];
+    for i in 0..m1.len() {
+        for j in 0..m1.len() {
+
+            let mat1 = _mm256_set1_ps(m1[i][j]);
+
+            for k in (0..m1.len()).step_by(8) {
+
+                let mat2 = _mm256_loadu_ps(&m2[j][k]);
+                let mut multi_res = _mm256_loadu_ps(&result[i][k]);
+                multi_res = _mm256_add_ps(multi_res, _mm256_mul_ps(mat1, mat2));
+
+                _mm256_storeu_ps(&mut result[i][k], multi_res);
+
+                // let ymm0 = _mm256_loadu_ps(&m1[i][k]);
+                // let ymm1 = _mm256_set_ps(m2[k + 7][j], m2[k + 6][j],
+                //                          m2[k + 5][j], m2[k + 4][j],
+                //                          m2[k + 3][j], m2[k + 2][j],
+                //                          m2[k + 1][j], m2[k + 0][j]);
+                // let ymm2 = _mm256_mul_ps(ymm0, ymm1);
+                // let high = _mm256_extractf128_ps(ymm2, 1);
+                // let sum = _mm_add_ps(_mm256_castps256_ps128(ymm2), high);
+                // let sum = _mm_hadd_ps(sum, sum);
+                // let sum = _mm_hadd_ps(sum, sum);
+                //
+                // result[i][j] += _mm_cvtss_f32(sum);
+            }
         }
     }
-    true
+    result
+}
+
+#[target_feature(enable = "sse,avx")]
+unsafe fn sum_my_simd(result: &mut Matrix, m: Matrix) {
+    for i in 0..m.len() {
+        for j in (0..m.len()).step_by(8) {
+            _mm256_storeu_ps(&mut result[i][j],
+                             _mm256_add_ps(_mm256_loadu_ps(&result[i][j]),
+                                           _mm256_loadu_ps(&m[i][j])));
+        }
+    }
+}
+
+fn compute_my_simd(result: &mut MegaMatrix, m1: &MegaMatrix, m2: &MegaMatrix) {
+    for i in 0..result.len() {
+        for j in 0..result.len() {
+            for k in 0..result.len() {
+                unsafe {
+                    sum_my_simd(&mut result[i][j], mul_my_simd(&m1[i][k], &m2[k][j]));
+                }
+            }
+        }
+    }
 }
 
 fn main() {
     assert_eq!(LITTLE_SIZE % 8, 0);
 
-    let m1: Matrix = rand::random();
-    let m2: Matrix = rand::random();
+    let m1 = new_mega_matrix();
+    let m2 = new_mega_matrix();
 
-    let res1 = multiply_matrices(&m1, &m2);
-    let res2 = multiply_matrices_avx(&m1, &m2);
+    let mut res1: Box<MegaMatrix> = vec![[[[0f32; LITTLE_SIZE]; LITTLE_SIZE]; SIZE]; SIZE]
+        .into_boxed_slice()
+        .try_into()
+        .unwrap();
+    let now = Instant::now();
+    compute_no_simd(&mut *res1, &*m1, &*m2);
+    let elapsed_time = now.elapsed();
+    println!("No SIMD took {}ms", elapsed_time.as_millis());
 
-    if compare_matrices(&res1, &res2) {
-        println!("Matrices are the same!");
+    let mut res2: Box<MegaMatrix> = vec![[[[0f32; LITTLE_SIZE]; LITTLE_SIZE]; SIZE]; SIZE]
+        .into_boxed_slice()
+        .try_into()
+        .unwrap();
+    let now = Instant::now();
+    compute_auto_simd(&mut *res2, &*m1, &*m2);
+    let elapsed_time = now.elapsed();
+    println!("Auto SIMD took {}ms", elapsed_time.as_millis());
+
+    if compare_mega_matrices(&res1, &res2) {
+        println!("res1=res2");
     }
 
-    // let m1 = new_mega_matrix();
-    // let m2 = new_mega_matrix();
-    // let mut m3: Box<MegaMatrix> = vec![[[[0f32; LITTLE_SIZE]; LITTLE_SIZE]; SIZE]; SIZE]
-    //     .into_boxed_slice()
-    //     .try_into()
-    //     .unwrap();
-    //
-    // let now = Instant::now();
-    // compute(&mut *m3, &*m1, &*m2);
-    // let elapsed_time = now.elapsed();
-    // println!("Compute took {}ms", elapsed_time.as_millis());
+    let mut res3: Box<MegaMatrix> = vec![[[[0f32; LITTLE_SIZE]; LITTLE_SIZE]; SIZE]; SIZE]
+        .into_boxed_slice()
+        .try_into()
+        .unwrap();
+    let now = Instant::now();
+    compute_my_simd(&mut *res3, &*m1, &*m2);
+    let elapsed_time = now.elapsed();
+    println!("My SIMD took {}ms", elapsed_time.as_millis());
+
+    if compare_mega_matrices(&res1, &res3) {
+        println!("res1=res3");
+    }
 }
