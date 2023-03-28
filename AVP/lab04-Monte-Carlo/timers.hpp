be0fca53ae0a8967c4a14754185d17b56de194a6
Andrey Sikorin <amaomi.prog@gmail.com>
2023-03-28 17:16:02 +0300

    Add events delete

--- AVP/lab04-Monte-Carlo/timers.hpp
+++ AVP/lab04-Monte-Carlo/timers.hpp
@@ -10,27 +10,24 @@ using namespace chrono;
 
 class TimerCPU {
 public:
-    ~TimerCPU()
-    {
+    ~TimerCPU() {
         const auto stop = high_resolution_clock::now();
         const auto elapsedTime = duration_cast<milliseconds>(stop - m_Start).count();
         cout << ", time=" << elapsedTime << "ms" << endl;
     }
 
 private:
-    time_point<high_resolution_clock> m_Start { high_resolution_clock::now() };
+    time_point<high_resolution_clock> m_Start{high_resolution_clock::now()};
 };
 
 class TimerGPU {
 public:
-    TimerGPU()
-    {
+    TimerGPU() {
         CUDA_ASSERT(cudaEventCreate(&m_Start))
         CUDA_ASSERT(cudaEventRecord(m_Start))
     }
 
-    ~TimerGPU()
-    {
+    ~TimerGPU() {
         cudaEvent_t stop;
         CUDA_ASSERT(cudaEventCreate(&stop))
         CUDA_ASSERT(cudaEventRecord(stop))
@@ -38,8 +35,10 @@ public:
         float elapsedTime;
         CUDA_ASSERT(cudaEventElapsedTime(&elapsedTime, m_Start, stop))
         cout << ", time=" << elapsedTime << "ms" << endl;
+        CUDA_ASSERT(cudaEventDestroy(m_Start))
+        CUDA_ASSERT(cudaEventDestroy(stop))
     }
 
 private:
-    cudaEvent_t m_Start {};
+    cudaEvent_t m_Start{};
 };
